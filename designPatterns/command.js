03fa2732aee256b3ef3bd07c1cff0e7e3a7f7dfc
ococa-15 <wangchao.profession@gmail.com>
2019-06-06 14:50:32 +0800

    command pattern

---
+++ designPatterns/command.js
@@ -0,0 +1,28 @@
+/**
+ * command pattern 
+ * 命令模式
+ * 简介： 命令模式旨在将方法调用，请求或操作封装到单一对象中，
+ *      从而根据我们的不同请求对客户端进行参数化和可供执行的方法调用
+ */
+
+/**
+ * 
+ */
+
+// var CarManager = (function() {
+//     var CarManager = {
+//         // 请求信息 request information
+//         requestInfo: function(model, id) {
+//             return "The information for " + model + " with ID " + id + " is foobar";
+//         },
+//         // purchase the car
+//         buyVehicle: function(model, id) {
+//             return "You have successfully purchased Item " + id + ", a " + model;
+//         },
+//         // arrange a viewing
+//         arrangeViewing: function( model, id ){
+//             return "You have successfully booked a viewing of " + model + " ( " + id + " ) ";
+//         }
+//     }
+// })();
+
